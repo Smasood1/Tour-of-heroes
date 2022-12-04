03e7244cefd0730b3c5b270f35755151b7768460
Smasood1 <saadmasood219@gmail.com>
2022-12-04 14:58:52 -0500

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr.Manhattan' },
+  { id: 13, name: 'GOKU' },
+  { id: 14, name: 'Batman' },
+  { id: 15, name: 'Superman' },
+  { id: 16, name: 'Eobard Thawne' },
+  { id: 17, name: 'Homelander' },
+  { id: 18, name: 'Thanos' },
+  { id: 19, name: 'Flash' },
+  { id: 20, name: 'Spiderman' }
+];
